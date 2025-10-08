172573ecc4a36c93c60c4104fae78c192934cc29
ysetreal <ysetreal@gmail.com>
2025-10-07 17:25:22 -0700

    day1:Hello c program

---
+++ day1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+char name[50];
+
+int age;
+
+printf("Enter your name: ");
+
+scanf("%s", name);
+
+printf("Enter your age: ");
+
+scanf("%d", age);
+
+printf("Hello, %s! You are %d Years old .\n", name, age);
+
+//Printf("My name is Isaac, I'm 23 years old, and I love programming.\n");
+
+return 0;
+
+}
